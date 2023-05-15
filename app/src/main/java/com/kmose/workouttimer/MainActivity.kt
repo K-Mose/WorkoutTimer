75907d0ca422db85fa583028249a8643cc539824
Mose <km_413@naver.com>
2023-05-15 14:30:16 +0900

    onBackPressed

--- app/src/main/java/com/kmose/workouttimer/MainActivity.kt
+++ app/src/main/java/com/kmose/workouttimer/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.kmose.workouttimer
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,17 +12,39 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.compose.rememberNavController
 import com.kmose.workouttimer.navigation.SetupNavigation
 import com.kmose.workouttimer.ui.theme.WorkoutTimerTheme
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+
+    var close = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             WorkoutTimerTheme {
                 val navController = rememberNavController()
                 SetupNavigation(navController = navController)
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        if (close) {
+            super.onBackPressed()
+            return
+        }
+        else {
+            close = true
+            Toast.makeText(this, "한 번 더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show()
+            lifecycleScope.launch(Dispatchers.IO) {
+                Thread.sleep(2000)
+                Log.d("MYTAG", "CLOSE FALSE")
+                close = false
             }
         }
     }
